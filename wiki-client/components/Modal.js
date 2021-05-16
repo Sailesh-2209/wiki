b70025bf53e8919925ee8b6caae5359e091ec3bf
Sailesh <saileshr2209@gmail.com>
2021-05-16 11:05:07 +0530

    added loading spinner after program creation

--- wiki-client/components/Modal.js
+++ wiki-client/components/Modal.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Modal from "react-modal";
+import ClipLoader from "react-spinners/ClipLoader";
 import { baseURL } from "../constants/baseURL";
 import styles from "../styles/Modal.module.css";
 
@@ -20,12 +21,14 @@ const customStyles = {
 
 Modal.setAppElement("body");
 
-export function MyModal({ isOpen, setIsOpen }) {
+export function MyModal({ isOpen, setIsOpen, setPrograms, programs }) {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [start, setStart] = useState("");
   const [end, setEnd] = useState("");
   const [imgAddress, setImgAddress] = useState("");
+  const [saving, setSaving] = useState(false);
+  const [newError, setNewError] = useState(null);
 
   const closeModal = () => {
     setIsOpen(false);
@@ -34,7 +37,7 @@ export function MyModal({ isOpen, setIsOpen }) {
   const handleSave = () => {
     let token = localStorage.getItem("jwt_token");
     let uid = localStorage.getItem("uid");
-    console.log(uid);
+    setSaving(true);
     axios
       .post(
         `${baseURL}/programs`,
@@ -54,9 +57,21 @@ export function MyModal({ isOpen, setIsOpen }) {
       )
       .then((value) => {
         console.log(value.data);
+        setSaving(false);
+        if (value.data.error !== null) {
+          setNewError(value.data.error);
+        } else {
+          setIsOpen(false);
+          setPrograms((programs) => {
+            console.log(value.data.program);
+            console.log(programs.concat(value.data.programs));
+            return programs.concat(value.data.programs);
+          });
+        }
       })
       .catch((error) => {
         console.log(error);
+        setSaving(false);
       });
   };
 
@@ -67,90 +82,101 @@ export function MyModal({ isOpen, setIsOpen }) {
       style={customStyles}
       contentLabel="Example Modal"
     >
-      <div className={styles.container}>
-        <div className={styles.headingContainer}>
-          <h2 className={styles.heading}>Add a new show</h2>
+      {saving ? (
+        <div className={styles.loader}>
+          <ClipLoader />
         </div>
-        <div className={styles.divider}></div>
-        <div className={styles.modalBody}>
-          <form className={styles.form}>
-            <div className={styles.name}>
-              <label className={styles.label} htmlFor="name">
-                Name
-              </label>
-              <input
-                className={styles.input}
-                type="text"
-                id="name"
-                name="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
+      ) : (
+        <>
+          <div className={styles.container}>
+            <div className={styles.headingContainer}>
+              <h2 className={styles.heading}>Add a new show</h2>
             </div>
-            <div className={styles.description}>
-              <label className={styles.label} htmlFor="description">
-                Description
-              </label>
-              <input
-                type="text"
-                id="description"
-                name="description"
-                className={styles.input}
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-              />
+            <div className={styles.divider}></div>
+            <div className={styles.modalBody}>
+              <form className={styles.form}>
+                <div className={styles.name}>
+                  <label className={styles.label} htmlFor="name">
+                    Name
+                  </label>
+                  <input
+                    className={styles.input}
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className={styles.description}>
+                  <label className={styles.label} htmlFor="description">
+                    Description
+                  </label>
+                  <input
+                    type="text"
+                    id="description"
+                    name="description"
+                    className={styles.input}
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
+                </div>
+              </form>
+              <div className={styles.dates}>
+                <label className={styles.label} htmlFor="start">
+                  Started in
+                </label>
+                <input
+                  type="text"
+                  id="start"
+                  name="start"
+                  className={styles.input}
+                  value={start}
+                  onChange={(e) => setStart(e.target.value)}
+                />
+              </div>
+              <div className={styles.dates}>
+                <label className={styles.label} htmlFor="end">
+                  Ended in
+                </label>
+                <input
+                  type="text"
+                  id="end"
+                  name="end"
+                  className={styles.input}
+                  value={end}
+                  onChange={(e) => setEnd(e.target.value)}
+                />
+              </div>
+              <div className={styles.image}>
+                <label className={styles.label} htmlFor="image">
+                  Image Address
+                </label>
+                <textarea
+                  type="text"
+                  id="image"
+                  name="image"
+                  className={styles.input}
+                  value={imgAddress}
+                  onChange={(e) => setImgAddress(e.target.value)}
+                />
+              </div>
             </div>
-          </form>
-          <div className={styles.dates}>
-            <label className={styles.label} htmlFor="start">
-              Started in
-            </label>
-            <input
-              type="text"
-              id="start"
-              name="start"
-              className={styles.input}
-              value={start}
-              onChange={(e) => setStart(e.target.value)}
-            />
+            <div className={styles.divider}></div>
+            <div className={styles.footer}>
+              <button
+                className={styles.saveButton}
+                onClick={() => handleSave()}
+              >
+                Save
+              </button>
+              <button className={styles.cancelButton} onClick={closeModal}>
+                Cancel
+              </button>
+            </div>
           </div>
-          <div className={styles.dates}>
-            <label className={styles.label} htmlFor="end">
-              Ended in
-            </label>
-            <input
-              type="text"
-              id="end"
-              name="end"
-              className={styles.input}
-              value={end}
-              onChange={(e) => setEnd(e.target.value)}
-            />
-          </div>
-          <div className={styles.image}>
-            <label className={styles.label} htmlFor="image">
-              Image Address
-            </label>
-            <textarea
-              type="text"
-              id="image"
-              name="image"
-              className={styles.input}
-              value={imgAddress}
-              onChange={(e) => setImgAddress(e.target.value)}
-            />
-          </div>
-        </div>
-        <div className={styles.divider}></div>
-        <div className={styles.footer}>
-          <button className={styles.saveButton} onClick={() => handleSave()}>
-            Save
-          </button>
-          <button className={styles.cancelButton} onClick={closeModal}>
-            Cancel
-          </button>
-        </div>
-      </div>
+        </>
+      )}
     </Modal>
   );
 }
--- wiki-client/components/Programs.js
+++ wiki-client/components/Programs.js
@@ -16,6 +16,7 @@ export function Programs({ programs, loggedIn, setPrograms }) {
         isOpen={isOpen}
         setIsOpen={setIsOpen}
         setPrograms={setPrograms}
+        programs={programs}
       />
       <div className={styles.programsContainer}>
         {loggedIn ? (
